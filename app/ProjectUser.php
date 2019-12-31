63ef6b51bbf3ecc7cf53ecda81e5a00a83dc6f0a
Kacper Kasper <kacperkasper@gmail.com>
2019-12-31 10:28:39 +0100

    Upgrade Laravel to 5.8

--- app/ProjectUser.php
+++ app/ProjectUser.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class ProjectUser extends Pivot
 {
+    /**
+    * Indicates if the IDs are auto-incrementing.
+    *
+    * @var bool
+    */
+    public $incrementing = true;
+
     public function language()
     {
         return $this->belongsTo('Polyglot\Language');
