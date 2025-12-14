6106f408a29a247c2926db14972190983108f605
Kacper Kasper <kacperkasper@gmail.com>
2025-12-14 19:52:39 +0100

    Limit number of displayed notifications
    * Bell menu is limited to 10 newest.
    * Paginate results in notification center.

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -9,7 +9,7 @@ class NotificationsController extends Controller
 {
     public function index()
     {
-        $notifications = Auth::user()->notifications;
+        $notifications = Auth::user()->notifications()->paginate(20);
         return view('notifications.index')->with('notifications', $notifications);
     }
 
